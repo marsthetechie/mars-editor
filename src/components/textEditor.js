1f948b1c6526411b678e9889117c301dca890d18
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-11-01 19:03:36 +0100

    add initial value for the editor

--- src/components/textEditor.js
+++ src/components/textEditor.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
 
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'My first paragraph!',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  }
+})
+
 export default class TextEditor extends Component {
 
-    state = {
-        value: '',
-    }
+  state = {
+    value: initialValue,
+  }
 
-    onChange = ({value}) => {
-        this.setState({value})
-    }
+  onChange = ({value}) => {
+    this.setState({value})
+  }
 
-    render() {
-        return (
-            <Editor value={this.state.value} onChange={this.onChange} />
-        )
-    }
+  render() {
+    return (
+      <Editor value={this.state.value} onChange={this.onChange} />
+    )
+  }
 }
