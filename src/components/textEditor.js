a98f970d91318200e9a4d60de9019349c80faee7
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-11-07 13:57:23 +0100

    finish plain text edit

--- src/components/textEditor.js
+++ src/components/textEditor.js
@@ -2,12 +2,6 @@ import React, { Component, Fragment } from 'react';
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
 
-import Icon from 'react-icons-kit';
-import { bold } from 'react-icons-kit/feather/bold';
-import { italic } from 'react-icons-kit/feather/italic';
-
-import { BoldMark, ItalicMark, FormatToolbar } from './index';
-
 const initialValue = Value.fromJSON({
   document: {
     nodes: [
@@ -19,7 +13,7 @@ const initialValue = Value.fromJSON({
             object: 'text',
             leaves: [
               {
-                text: 'My first paragraph!',
+                text: 'Some dumb dummy text',
               },
             ],
           },
@@ -38,59 +32,10 @@ export default class TextEditor extends Component {
   onChange = ({value}) => {
     this.setState({value})
   }
-
-  onKeyDown = (e, change) => {
-    
-    if (!e.ctrlKey) { return }
-    e.preventDefault()
-
-    switch (e.key) {
-
-      case 'b': {
-        change.toggleMark('bold')
-        return true
-      }
-      case 'i': {
-        change.toggleMark('italic')
-        return true
-      }
-      default: {
-        return;
-      }
-    }
-  }
-
-  renderMark = props => {
-    switch (props.mark.type) {
-      case 'bold':
-        return <BoldMark {...props} />
-
-        case 'italic':
-        return <ItalicMark {...props} />
-
-        default:
-        return;
-    }
-  }
-
+  
   render() {
     return (
-      <Fragment>
-        <FormatToolbar>
-          <button className="tooltip-icon-button">
-            <Icon icon={bold} />
-          </button>
-          <button className="tooltip-icon-button">
-            <Icon icon={italic} />
-          </button>
-        </FormatToolbar>
-        <Editor 
-      value={this.state.value} 
-      onChange={this.onChange} 
-      onKeyDown={this.onKeyDown} 
-      renderMark={this.renderMark} 
-      />
-      </Fragment>
+      <Editor value={this.state.value} onChange={this.onChange} />
     );
   }
 }
